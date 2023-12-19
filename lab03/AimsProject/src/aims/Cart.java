95ba9f6872b0bcad0a6b0f60cb09f367e4b92dc0
Mitchell Vu <mitchell.vu0208@gmail.com>
2023-12-20 00:55:02 +0700

    config: AIMS Project base for Lab 03

---
+++ lab03/AimsProject/src/aims/Cart.java
@@ -0,0 +1,51 @@
+/**
+ * @project AimsProject
+ * @author Vu Minh Hieu
+ * @since November 2023
+ */
+
+package aims;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private final DigitalVideoDisc[] itemsOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int quantityOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        // Check the cart is already full or not
+        if (quantityOrdered >= MAX_NUMBERS_ORDERED) {
+            System.out.println("The cart is full.");
+            return;
+        }
+
+        this.itemsOrdered[this.quantityOrdered++] = disc;
+        System.out.printf("The disc %s has been added.\n", disc.getTitle());
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (quantityOrdered == 0) {
+            System.out.println("The cart is empty.");
+            return;
+        }
+
+        for (int i = 0; i < quantityOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                for (int j = i; j < quantityOrdered - 1; j++)
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                itemsOrdered[--quantityOrdered] = null;
+                break;
+            }
+        }
+
+        System.out.printf("The disc %s has been removed.\n", disc.getTitle());
+    }
+
+    public float totalCost() {
+        float cost = 0;
+
+        for (int i = 0; i < quantityOrdered; i++) {
+            cost += itemsOrdered[i].getCost();
+        }
+        return cost;
+    }
+}
