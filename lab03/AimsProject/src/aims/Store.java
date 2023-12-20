1ad6aa64423e988e43588d8510bea6f794f78ddf
Mitchell Vu <mitchell.vu0208@gmail.com>
2023-12-20 11:03:19 +0700

    topic: Lab 03 - Store

---
+++ lab03/AimsProject/src/aims/Store.java
@@ -0,0 +1,58 @@
+/**
+ * @project AimsProject
+ * @author Mitchell Vu
+ * @since November 2023
+ */
+
+package aims;
+
+public class Store {
+    private static final int DEFAULT_CAPACITY = 20;
+    private final DigitalVideoDisc[] itemsInStore;
+    private int itemCount;
+
+    public Store() {
+        this.itemsInStore = new DigitalVideoDisc[DEFAULT_CAPACITY];
+    }
+
+    public Store(int capacity) {
+        this.itemsInStore = new DigitalVideoDisc[capacity];
+    }
+
+    public void addDVD(DigitalVideoDisc... dvds) {
+        for (DigitalVideoDisc dvd : dvds) {
+            if (itemCount < itemsInStore.length) {
+                itemsInStore[itemCount] = dvd;
+                itemCount++;
+                System.out.println("DVD added to the store: " + dvd.getTitle());
+            } else {
+                System.out.println("Store is full. Cannot add more DVDs.");
+            }
+        }
+    }
+
+    public void removeDVD(String... titles) {
+        for (String title : titles) {
+            for (int i = 0; i < itemCount; i++) {
+                if (itemsInStore[i].getTitle().equals(title)) {
+                    System.out.println("DVD removed from the store: " + title);
+//                    Move the last item to the position of the removed item
+                    itemsInStore[i] = itemsInStore[itemCount - 1];
+//                    Clear the last position
+                    itemsInStore[itemCount - 1] = null;
+                    itemCount--;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void print() {
+        System.out.println("***********************STORE***********************");
+        System.out.println("Items in the store:");
+        for (int i = 0; i < itemCount; i++) {
+            System.out.printf("%d. %s\n", i + 1, itemsInStore[i].toString());
+        }
+        System.out.println("*****************************************************");
+    }
+}
---
+++ lab03/AimsProject/src/aims/StoreTest.java
@@ -0,0 +1,36 @@
+/**
+ * @project AimsProject
+ * @author Mitchell Vu
+ * @since November 2023
+ */
+
+package aims;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        Store store = new Store(20);
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc(
+                "The Lion King",
+                "Animation",
+                "Roger Allers",
+                87,
+                19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc(
+                "Star Wars",
+                "Science Fiction",
+                "George Lucas",
+                87,
+                24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc(
+                "Aladin",
+                "Animation",
+                18.99f);
+
+        store.addDVD(dvd1, dvd2, dvd3);
+        store.print();
+
+        store.removeDVD("Aladin");
+        store.print();
+    }
+}
